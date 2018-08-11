0cbb7127948ff9d324382df2d81e0fc29d339389
lijiawei <372745456@qq.com>
2018-08-11 10:22:39 +0800

    【加入Priority Replay】 效果不错

--- RL/Memory.py
+++ RL/Memory.py
@@ -1,37 +1,55 @@
-from collections import deque
 import logging
-
-import numpy as np
 import os
 
+import numpy as np
 
-class ReplayMemory(object):
-    def __init__(self):
-        """
-        记忆库存储格式：
-        observation
-        action
-        reward
-        next_observation
-        done
-        """
-        self.memory = deque()
+from .sumTree import SumTree
 
-    def __len__(self):
-        return len(self.memory)
 
-    def push_back(self, obj):
-        self.memory.append(obj)
+class ReplayMemory:
 
-    def pop_front(self):
-        self.memory.popleft()
+    def __init__(self, mem_size, alpha, epsilon):
+        self._ALPHA = alpha
+        self._EPSILON = epsilon
+
+        self._tree = SumTree(mem_size)
+
+    def store(self, transition):
+        max_priority = self._tree.max_leaf()
+        if max_priority == 0:
+            max_priority = self._priority(0)
+        self._tree.insert(transition, max_priority)
+
+    def sample(self, num_samples, beta):
+        batch = [None] * num_samples
+        IS_weights = np.zeros((num_samples,))  # Importance-sampling (IS) weights
+        tree_indices = np.zeros((num_samples,), dtype=np.int32)
+
+        len_seg = self._tree.sum() / num_samples
+        min_prob = self._tree.min_leaf() / self._tree.sum()
+
+        for i in range(num_samples):
+            val = np.random.uniform(len_seg * i, len_seg * (i + 1))
+            tree_indices[i], priority, batch[i] = self._tree.retrieve(val)
+            prob = priority / self._tree.sum()
+            IS_weights[i] = np.power(prob / min_prob, -beta)  # Simplified formula
+
+        return batch, IS_weights, tree_indices
+
+    def update(self, tree_indices, abs_td_errs):
+        priorities = self._priority(abs_td_errs)
+        for idx, priority in zip(tree_indices, priorities):
+            self._tree.update(idx, priority)
+
+    def _priority(self, abs_td_err):
+        return np.power(abs_td_err + self._EPSILON, self._ALPHA)
 
     def dump(self, path="data"):
-        observation = [data[0] for data in self.memory]
-        action = [data[1] for data in self.memory]
-        reward = [data[2] for data in self.memory]
-        next_observation = [data[3] for data in self.memory]
-        done = [data[4] for data in self.memory]
+        observation = [data[0] for data in self._tree.data]
+        action = [data[1] for data in self._tree.data]
+        reward = [data[2] for data in self._tree.data]
+        next_observation = [data[3] for data in self._tree.data]
+        done = [data[4] for data in self._tree.data]
 
         if not os.path.exists(path):
             os.mkdir(path)
@@ -50,10 +68,6 @@ class ReplayMemory(object):
             done = np.load(os.path.join(path, "done.npz"))
 
             for e in observation:
-                self.memory.append((observation[e], action[e], reward[e], next_observation[e], done[e]))
+                self.store((observation[e], action[e], reward[e], next_observation[e], done[e]))
         except:
             logging.exception("load failed.")
-
-
-if __name__ == "__main__":
-    pass
